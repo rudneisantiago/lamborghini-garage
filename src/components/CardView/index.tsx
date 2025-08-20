528cea4b9bdd1558c38c1594780ef54b5744bb2a
Rudnei Santiago <rudnei.santiago@gmail.com>
2025-08-20 11:16:47 -0300

    render details and car image

--- src/components/CardView/index.tsx
+++ src/components/CardView/index.tsx
@@ -1,8 +1,9 @@
-import { View, Text, Button, Image } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 import { styles } from './styles';
 import Logo from '../../../assets/logo.png'
 import { Divider } from '../Divider';
+import { CAR_ASSETS_BASE_URL } from '../../constants/car';
 
 export function CardView() {
   const renderLogoBox = () => (
@@ -11,10 +12,26 @@ export function CardView() {
     </View>
   )
 
+  const renderCarDetails = () => (
+    <View style={styles.carDetailsContainer}>
+      <Text style={styles.carBrand}>Lamborghini</Text>
+      <Text style={styles.carName}>Modelo do carro</Text>
+    </View>
+  )
+
+  const renderCarImage = () => (
+    <Image
+      style={styles.image}
+      source={{ uri: `${CAR_ASSETS_BASE_URL}1.png` }}
+    />
+  )
+
   return (
     <View style={styles.container}>
       {renderLogoBox()}
       <Divider />
+      {renderCarDetails()}
+      {renderCarImage()}
     </View>
   );
 }
--- src/components/CardView/styles.tsx
+++ src/components/CardView/styles.tsx
@@ -14,9 +14,29 @@ export const styles = StyleSheet.create({
     height: 200,
     resizeMode: 'contain'
   },
+  carDetailsContainer: {
+    alignItems: 'center'
+  },
+  image: {
+    flex: 1,
+    marginBottom: 10,
+    resizeMode: 'contain'
+  },
   imageLogo: {
     flex: 1,
     width: '65%',
     resizeMode: 'contain'
-  }
+  },
+  carBrand: {
+    color: '#FFF',
+    fontWeight: 400,
+    fontSize: 18,
+    fontStyle: 'italic'
+  },
+  carName: {
+    color: '#FFF',
+    fontSize: 20,
+    fontWeight: 700,
+    textAlign: 'center'
+  },
 });
